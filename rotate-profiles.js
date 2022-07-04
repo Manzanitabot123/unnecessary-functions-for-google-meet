217fee3b60cbe08f9cbc87023754def3df92539a
Manzanitabot123 <84603580+Manzanitabot123@users.noreply.github.com>
2022-07-04 16:23:59 -0500

    Update :D

--- rotate-profiles.js
+++ rotate-profiles.js
@@ -1,6 +1,6 @@
 var css = '@keyframes rotation {from { transform: rotate(0deg); } to { transform: rotate(359deg); }}', head = document.head || document.getElementsByTagName('head')[0], style = document.createElement('style');
 head.appendChild(style); style.type = 'text/css';
-if (style.styleSheet){ style.styleSheet.cssText = css;} else { style.appendChild(document.createTextNode(css)) }; for(x=0; x<document.querySelectorAll("[class='qg7mD r6DyN xm86Be JBY0Kc eXUaib']").length; x++) { document.querySelectorAll("[class='qg7mD r6DyN xm86Be JBY0Kc eXUaib']")[x].style.animation = "rotation 0.7s infinite linear"; }
+if (style.styleSheet){ style.styleSheet.cssText = css;} else { style.appendChild(document.createTextNode(css)) }; for(x=0; x<document.querySelectorAll("img").length; x++) { document.querySelectorAll("img")[x].style.animation = "rotation 0.7s infinite linear"; }
 
 /*
 To disable just copy this:
