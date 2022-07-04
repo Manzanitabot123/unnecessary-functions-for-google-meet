0f677a2f428d6ba4b2eb6c13113609e7b5b505cc
Manzanitabot123 <84603580+Manzanitabot123@users.noreply.github.com>
2022-07-04 09:32:33 -0500

    get all user profiles in hd

---
+++ get-all-profiles.js
@@ -0,0 +1,1 @@
+for(x=0; x<document.querySelectorAll("[class='qg7mD r6DyN xm86Be JBY0Kc eXUaib']").length; x++) { console.log(`Nombre: "${document.querySelectorAll("[class='XEazBc adnwBd']")[x].textContent} | Perfil: ${document.querySelectorAll("[class='qg7mD r6DyN xm86Be JBY0Kc eXUaib']")[x].src.replace("s160-", "s2560-")} | N°${x}`) }  
